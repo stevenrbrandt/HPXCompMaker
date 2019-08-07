4bf6ed3f55580d6b50d4cfb2ea2f0861102ddda9
Steven R. Brandt <sbrandt@cct.lsu.edu>
2019-08-07 16:10:51 +0000

    Update code

--- PrintUtil.py
+++ PrintUtil.py
@@ -4,7 +4,11 @@ from comp import *
 class PrintUtil:
     def hello(self) -> None:
         cplusplus('std::cout << "Hello" << std::endl;')
+    def hello2() -> None:
+        pass
     def write(self, s : str) -> None:
         cplusplus('std::cout << s << std::endl;')
     def __init__(self):
         cplusplus("/*comment*/")
+    def __del__(self):
+        pass
