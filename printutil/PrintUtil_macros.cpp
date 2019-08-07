b3a53ac70cfa60a5554137caaf0ea65d7c752031
Steven R. Brandt <sbrandt@cct.lsu.edu>
2019-08-07 17:23:47 +0000

    Add the generated files

---
+++ printutil/PrintUtil_macros.cpp
@@ -0,0 +1,18 @@
+#include <PrintUtil.hpp>
+HPX_REGISTER_COMPONENT_MODULE();
+
+typedef hpx::components::component<
+    printutil::server::PrintUtil
+> PrintUtil_type;
+
+HPX_REGISTER_COMPONENT(PrintUtil_type, PrintUtil);
+        
+HPX_REGISTER_ACTION(
+    printutil::server::PrintUtil::hello_action, PrintUtil_hello_action);
+            
+HPX_REGISTER_ACTION(
+    printutil::server::PrintUtil::hello2_action, PrintUtil_hello2_action);
+            
+HPX_REGISTER_ACTION(
+    printutil::server::PrintUtil::write_action, PrintUtil_write_action);
+            
